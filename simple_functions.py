02dda737b688e61bb8f4813873f3a1609f330bf7
Sai Siddartha D <sai.siddartha@tigeranalytics.com>
2024-03-15 14:00:05 +0530

    Fixed the issue to modify the code and simplified the script

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,11 @@
 # Custom python functions
 
 def double_number(a):
+    print(f'value before double_number(): {a}')
+    print(f'value after double_number(): {a+a}')
     return a+a
 
 def square_number(a):
+    print(f'value before square_number(): {a}')
+    print(f'value after square_number(): {a*a}')
     return a*a
