c816de75a549e6548f99808c5f07f68156b2323c
hemasree <hemasree.konduru@tigeranalytics.com>
2024-03-15 15:10:42 +0530

    ENH:Modified docstrings

--- simple_functions.py
+++ simple_functions.py
@@ -2,14 +2,14 @@
 
 """
 The function double_number adds the given number to itself.
-It takes one argument.
+It takes an integer value as an argument.
 """
 def double_number(a):
     return a+a
 
 """
 The function square_number multiplies the given number to itself.
-It takes one argument.
+It takes an integer value as an argument.
 """
 
 def square_number(a):
